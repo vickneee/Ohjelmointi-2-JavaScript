b8cb5a243382ca236ff0d901c2b796bc6a0011fa
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-03 23:18:57 +0200

    Mod01 Teht5 js corr and ids renaming

--- mod-01/mod01-Teht5/script.js
+++ mod-01/mod01-Teht5/script.js
@@ -1,9 +1,11 @@
 `use strict`;
 
+const resultId = document.getElementById("resultId");
+
 const year = prompt("Give a year: ");
 
 function isLeapYear(year) {
   return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
 }
 
-document.getElementById("result").innerHTML = isLeapYear(year) ? `The year ${year} is a leap year.` : `The year ${year} is not a leap year.`;
+resultId.innerHTML = isLeapYear(year) ? `The year ${year} is a leap year.` : `The year ${year} is not a leap year.`;
