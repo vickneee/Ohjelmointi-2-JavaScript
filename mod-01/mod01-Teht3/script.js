2ed43ce36809062815b43a63f640346b05c001cc
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-03 23:15:45 +0200

    Mod01 Teht3 js corr

--- mod-01/mod01-Teht3/script.js
+++ mod-01/mod01-Teht3/script.js
@@ -5,9 +5,9 @@ const summationId = document.getElementById("summationId");
 const productId = document.getElementById("productId");
 const averageId = document.getElementById("averageId");
 
-let num1 = parseInt(prompt(`Type a first number:`));
-let num2 = parseInt(prompt(`Type a second number:`))
-let num3 = parseInt(prompt(`Type a third number:`))
+const num1 = parseInt(prompt(`Type a first number:`));
+const num2 = parseInt(prompt(`Type a second number:`))
+const num3 = parseInt(prompt(`Type a third number:`))
 
 function summation(num1, num2, num3) {
   return num1 + num2 + num3;
@@ -21,9 +21,9 @@ function average(num1, num2, num3) {
   return summation(num1, num2, num3) / 3;
 }
 
-let summationResult = summation(num1, num2, num3);
-let productResult = product(num1, num2, num3);
-let averageResult = average(num1, num2, num3);
+const summationResult = summation(num1, num2, num3);
+const productResult = product(num1, num2, num3);
+const averageResult = average(num1, num2, num3);
 
 target.innerHTML = `The inserted numbers were ${num1}, ${num2} and ${num3}.`
 summationId.innerHTML = `The sum of the numbers is ${summationResult}.`
