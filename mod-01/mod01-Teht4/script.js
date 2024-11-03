55df9d03c8f496457ba29bbf7caf356635b2b5c2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-04 00:15:51 +0200

    Mod01 Teht4 js corr and html corr

--- mod-01/mod01-Teht4/script.js
+++ mod-01/mod01-Teht4/script.js
@@ -1,31 +1,37 @@
 `use strict`;
 
-const welcomeId = document.getElementById("welcomeId");
-const resultId = document.getElementById("resultId");
-
-const name = prompt(`Give your name:`);
-
 function randomClass() {
-  const randomClasses = Math.floor(Math.random() * 4) + 1;
-  let chosenClass;
-  switch(randomClasses) {
-    case 1:
-      chosenClass = `Gryffindor`;
-      break;
-    case 2:
-      chosenClass = `Slytherin`;
-      break;
-    case 3:
-      chosenClass = `Hufflepuff`;
-      break;
-    case 4:
-      chosenClass = `Ravenclaw`;
-      break;
+  const welcomeId = document.getElementById('welcomeId');
+  const resultId = document.getElementById('resultId');
+  
+  const name = prompt(`Give your name:`);
+  
+  if (name === null || name === '') {
+    alert(`Please insert your name.`);
+    return randomClass();
   }
-  return chosenClass;
+  else {
+    const randomClasses = Math.floor(Math.random() * 4) + 1;
+    let chosenClass;
+    switch (randomClasses) {
+      case 1:
+        chosenClass = `Gryffindor`;
+        break;
+      case 2:
+        chosenClass = `Slytherin`;
+        break;
+      case 3:
+        chosenClass = `Hufflepuff`;
+        break;
+      case 4:
+        chosenClass = `Ravenclaw`;
+        break;
+    }
+    let result = chosenClass;
+    welcomeId.innerHTML = `Welcome to Hogwarts, ${name}!`;
+    resultId.innerHTML = `Our four classes are Gryffindor, Slytherin, Hufflepuff, and Ravenclaw.<br>
+    ${name}, you have been assigned to the ${result} class.`;
+  }
 }
 
-const result = randomClass();
-
-welcomeId.innerHTML = `Welcome to Hogwarts, ${name}!`;
-resultId.innerHTML = `${name}, you have been assigned to the ${result} class.`;
+randomClass();
