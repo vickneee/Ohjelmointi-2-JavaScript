0d0a0877878b43298a1c3315919b3c52bbc60010
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-03 21:48:36 +0200

    Mod01 Teht7 function corr

--- mod-01/mod01-Teht7/script.js
+++ mod-01/mod01-Teht7/script.js
@@ -4,21 +4,28 @@ function summation(number) {
   let sum = 0;
   for (let i = 0; i < number; i++) {
     let randomResult = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("diceRoll").innerHTML += `${randomResult} `;
-    sum += randomResult
+    document.getElementById('diceRoll').innerHTML += `${randomResult} `;
+    sum += randomResult;
   }
-  return sum
+  return sum;
 }
 
 function rollDice() {
-  const diceRolls = parseInt(prompt(`How many times do you want to roll the dice?`));
+  const diceRolls = parseInt(
+      prompt(`How many times do you want to roll the dice?`));
   
   if (diceRolls < 1) {
-    alert("Please insert number greater than 1.")
-  } else {
-    let sum = summation(diceRolls)
-    document.getElementById("result").innerHTML = `Sum of ${diceRolls} dice rolls is ${sum}.`
+    alert('Please insert number greater than 1.');
+  }
+  else if (diceRolls > 1) {
+      let sum = summation(diceRolls);
+      document.getElementById(
+          'result').innerHTML = `Sum of ${diceRolls} dice rolls is ${sum}.`;
+    }
+  else {
+    document.getElementById(
+        'result').innerHTML = `The dice is not rolled.`;
   }
 }
 
-rollDice()
+rollDice();
