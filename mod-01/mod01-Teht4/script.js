cd5f8febfb7611a0354328db28b1ea20dd440975
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-03 18:11:46 +0200

    Mod01 Teht4 Initial commit

---
+++ mod-01/mod01-Teht4/script.js
@@ -0,0 +1,28 @@
+`use strict`;
+
+const name = prompt(`Give your name:`);
+
+function randomClass() {
+  const randomClasses = Math.floor(Math.random() * 4) + 1;
+  let chosenClass;
+  switch(randomClasses) {
+    case 1:
+      chosenClass = `Gryffindor`;
+      break;
+    case 2:
+      chosenClass = `Slytherin`;
+      break;
+    case 3:
+      chosenClass = `Hufflepuff`;
+      break;
+    case 4:
+      chosenClass = `Ravenclaw`;
+      break;
+  }
+  return chosenClass;
+}
+
+result = randomClass();
+
+document.getElementById("welcome").innerHTML = `Welcome ${name}!`;
+document.getElementById("result").innerHTML = `${name} as a new student you have been assigned to the ${result} class.`;
