0b194ceabc804944b0ea5f01af00063d59ea7ca3
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-03 22:47:52 +0200

    Mod01 Teht3 js corr and ids renaming

--- mod-01/mod01-Teht3/script.js
+++ mod-01/mod01-Teht3/script.js
@@ -1,8 +1,13 @@
 `use strict`;
 
-const num1 = parseInt(prompt(`Type a first number:`));
-const num2 = parseInt(prompt(`Type a second number:`))
-const num3 = parseInt(prompt(`Type a third number:`))
+const target = document.getElementById("target");
+const summationId = document.getElementById("summationId");
+const productId = document.getElementById("productId");
+const averageId = document.getElementById("averageId");
+
+let num1 = parseInt(prompt(`Type a first number:`));
+let num2 = parseInt(prompt(`Type a second number:`))
+let num3 = parseInt(prompt(`Type a third number:`))
 
 function summation(num1, num2, num3) {
   return num1 + num2 + num3;
@@ -16,11 +21,11 @@ function average(num1, num2, num3) {
   return summation(num1, num2, num3) / 3;
 }
 
-summa = summation(num1, num2, num3);
-product = product(num1, num2, num3);
-average = average(num1, num2, num3);
+let summationResult = summation(num1, num2, num3);
+let productResult = product(num1, num2, num3);
+let averageResult = average(num1, num2, num3);
 
-document.getElementById("target").innerHTML = `The inserted numbers were ${num1}, ${num2} and ${num3}.`
-document.getElementById("summation").innerHTML = `The sum of the numbers is ${summa}.`
-document.getElementById("product").innerHTML = `The product of the numbers is ${product}.`
-document.getElementById("average").innerHTML = `The average of the numbers is ${average}.`
+target.innerHTML = `The inserted numbers were ${num1}, ${num2} and ${num3}.`
+summationId.innerHTML = `The sum of the numbers is ${summationResult}.`
+productId.innerHTML = `The product of the numbers is ${productResult}.`
+averageId.innerHTML = `The average of the numbers is ${averageResult.toFixed(2)}.`
