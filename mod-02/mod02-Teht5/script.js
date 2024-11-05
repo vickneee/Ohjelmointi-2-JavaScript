7eccc29af1215b2efd06307c2d7f8850039db3a3
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-05 23:36:59 +0200

    Mod02 Teht5 Initial commit

--- mod-02/mod02-Teht5/script.js
+++ mod-02/mod02-Teht5/script.js
@@ -1,2 +1,29 @@
 `use strict`;
 
+function numbersGiven() {
+  const targetId = document.getElementById('targetId');
+  let numbers = [];
+  let number;
+  
+  do {
+    number = parseInt(prompt('Give a number (0 to stop):'));
+    if (!isNaN(number)) {
+      if (numbers.includes(number)) {
+        alert(`The number ${number} has already been given!`);
+        break;
+      }
+      numbers.push(number);
+    }
+    else {
+      alert('Please give a number!');
+    }
+  } while (number !== 0) ;
+  
+  numbers.pop(); // remove the last zero
+  
+  numbers.sort((a, b) => a - b);
+  
+  targetId.innerHTML = numbers.join(', ');
+}
+
+numbersGiven();
