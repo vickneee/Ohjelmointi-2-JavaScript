8b6fda1de2bb7043a416c5bd03ffa9a34f360be5
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-05 16:36:03 +0200

    Initial commit

--- mod-01/mod01-Teht9/script.js
+++ mod-01/mod01-Teht9/script.js
@@ -1,32 +1,32 @@
 `use strict`;
 
-// number = 101;
-
 function isPrime() {
   const number = parseInt(prompt(`Give a number: `));
   const targetId = document.getElementById('targetId');
   
   if (number === 1) {
-    // console.log(`No, ${number} is not a prime number.`);
     targetId.innerHTML = `No, ${number} is not a prime number.`;
+    console.log(`No, ${number} is not a prime number.`);
   }
-  
   else if (number > 1) {
-    
-    for (let i = 2; i < number; i++) {
-      if (number % i === 0) {
-        return targetId.innerHTML = `${number} is NOT prime`;
-        // console.log(`${number} is NOT Prime`);
-        // break;
+      let isPrime = true;
+      for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        targetId.innerHTML = `${number} is prime.`;
+        console.log(`${number} is Prime`);
+      } else {
+        targetId.innerHTML = `${number} is NOT prime.`;
+        console.log(`${number} is NOT Prime`);
       }
     }
+    else if (number <= 0) {
+      targetId.innerHTML = `Please enter an positive number.`;
     }
-  else {
-      return targetId.innerHTML = `${number} is prime.`;
-      // console.log(`${number} is Prime`);
-      // break;
-    }
-    // return targetId.innerHTML = `${number} is prime.`;
 }
 
 isPrime();
