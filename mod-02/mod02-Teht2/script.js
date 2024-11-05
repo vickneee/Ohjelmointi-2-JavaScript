3b83bbe70ada95680e335573d8b57a9242b2d256
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-05 19:21:37 +0200

    Mod02 Teht2 Initial commit

--- mod-02/mod02-Teht2/script.js
+++ mod-02/mod02-Teht2/script.js
@@ -1,2 +1,28 @@
 `use strict`;
 
+function participants() {
+  const targetOl = document.getElementById("targetOl");
+
+  const participants = [];
+  const numberOfParticipants = parseInt(prompt(`How many participants are there?`));
+  
+  for (let i = 0; i < numberOfParticipants; i++) {
+    participants.push(prompt(`Enter the participants name:`));
+  }
+  
+  participants.sort();
+  
+  // for (let i= 0; i < participants.length; i++) {
+  //   const li = document.createElement("li");
+  //   li.textContent = participants[i];
+  //   targetOl.appendChild(li);
+  // }
+  
+  participants.forEach(participant => {
+    const li = document.createElement('li');
+    li.textContent = participant;
+    targetOl.appendChild(li);
+  });
+}
+
+participants();
