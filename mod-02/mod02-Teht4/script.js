c700ed9663286706cc9292aa291910edf07eef0e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-05 23:02:39 +0200

    Mod02 Teht4 Initial commit

--- mod-02/mod02-Teht4/script.js
+++ mod-02/mod02-Teht4/script.js
@@ -1,2 +1,26 @@
 `use strict`;
 
+function largestToTheSmallest() {
+  const targetId = document.getElementById("targetId");
+  let numbers = [];
+  let number;
+  
+  do {
+    number = parseInt(prompt("Give a number (0 to stop):"));
+    if (!isNaN(number)) {
+      numbers.push(number);
+    }
+    else {
+    alert("Please give a number!");
+  }
+  } while (number !== 0);
+  
+  numbers.pop(); // remove the last zero
+  
+  numbers.sort((a, b) => b - a);
+  
+  targetId.innerHTML = numbers.join(", ");
+  console.log(numbers);
+}
+
+largestToTheSmallest();
