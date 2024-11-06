fe9f7417eedf8c2e6efaf78401e5f0ed14eb613e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-06 02:41:02 +0200

    Mod02 Teht4 Initial commit

--- mod-02/mod02-Teht4/script.js
+++ mod-02/mod02-Teht4/script.js
@@ -1,25 +1,25 @@
 `use strict`;
 
 function largestToTheSmallest() {
-  const targetId = document.getElementById("targetId");
+  const targetId = document.getElementById('targetId');
   let numbers = [];
   let number;
   
   do {
-    number = parseInt(prompt("Give a number (0 to stop):"));
+    number = parseInt(prompt('Give a number (0 to stop):'));
     if (!isNaN(number)) {
       numbers.push(number);
     }
     else {
-    alert("Please give a number!");
-  }
+      alert('Please give a number!');
+    }
   } while (number !== 0);
   
   numbers.pop(); // remove the last zero
   
   numbers.sort((a, b) => b - a);
   
-  targetId.innerHTML = numbers.join(", ");
+  targetId.innerHTML = numbers.join(', ');
   console.log(numbers);
 }
 
