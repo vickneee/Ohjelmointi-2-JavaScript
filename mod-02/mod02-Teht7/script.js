8a2414e70b175fe44cac0b474471f7bde8aa06f6
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-07 16:08:26 +0200

    Mod02 Teht7 Initial commit

--- mod-02/mod02-Teht7/script.js
+++ mod-02/mod02-Teht7/script.js
@@ -1,7 +1,5 @@
 `use strict`;
 
-// Modify the function above so that it gets the number of sides on the dice as a parameter. With the modified function you can for example roll a 21-sided role-playing dice. The difference to the last exercise is that the dice rolling in the main program continues until the program gets the maximum number on the dice, which is asked from the user at the beginning. (2p)
-
 function rollDice(sides) {
   return Math.floor(Math.random() * sides) + 1;
 }
