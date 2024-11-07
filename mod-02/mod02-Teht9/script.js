3451a92486d16abc07f6fa0e8a15ccba55867329
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-07 17:06:05 +0200

    Mod02 Teht9 Initial commit

--- mod-02/mod02-Teht9/script.js
+++ mod-02/mod02-Teht9/script.js
@@ -1,2 +1,29 @@
 `use strict`;
 
+function even(arr) {
+  let evenArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenArr.push(arr[i]);
+    }
+  }
+  return evenArr;
+  
+}
+
+function main() {
+  const oldArrayId = document.getElementById('oldArrayId');
+  const newEvenArrayId = document.getElementById('newEvenArrayId');
+  
+  const array = [2, 7, 4, 9, 5, 8];
+  const evenArray = even(array);
+  
+  console.log(array);
+  console.log(evenArray);
+  
+  oldArrayId.innerHTML = array.toString();
+  newEvenArrayId.innerHTML = evenArray.toString();
+}
+
+main();
