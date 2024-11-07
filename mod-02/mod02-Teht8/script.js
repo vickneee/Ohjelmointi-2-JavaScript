5127ad5d9e408d24843b68ff245c9e83f818a6ad
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-07 16:46:26 +0200

    Mod02 Teht8 Initial commit

--- mod-02/mod02-Teht8/script.js
+++ mod-02/mod02-Teht8/script.js
@@ -1,2 +1,18 @@
 `use strict`;
 
+function concat(arrOfStrings) {
+  let result = '';
+  
+  for (let i = 0; i < arrOfStrings.length; i++) {
+    result += arrOfStrings[i];
+  }
+  return result;
+}
+
+function main() {
+  const targetId = document.getElementById('targetId');
+  const hardCodedArrOfStrings = ['Johnny', 'DeeDee', 'Joey', 'Marky'];
+  targetId.innerHTML = concat(hardCodedArrOfStrings);
+}
+
+main();
