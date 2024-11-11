52c1fa541fb34d0c7831432733b2534d9ab3ed0f
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 00:11:40 +0200

    Mod03 Teht2 Initial commit

--- mod-03/mod03-Teht2/script.js
+++ mod-03/mod03-Teht2/script.js
@@ -1,2 +1,17 @@
 `use strict`;
 
+const targetElement = document.querySelector('#target');
+
+const li1 = document.createElement('li');
+li1.textContent = `First item`;
+
+const li2 = document.createElement(`li`);
+li2.textContent = `Second item`;
+li2.classList.add(`my-item`);
+
+const li3 = document.createElement(`li`);
+li3.innerHTML = `Third item`;
+
+targetElement.appendChild(li1);
+targetElement.appendChild(li2);
+targetElement.appendChild(li3);
