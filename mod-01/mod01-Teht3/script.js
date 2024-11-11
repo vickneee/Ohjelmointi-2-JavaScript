ecd384340571f04d008db483315740dcfad84d9b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-11 12:57:49 +0200

    Semicolon added

--- mod-01/mod01-Teht3/script.js
+++ mod-01/mod01-Teht3/script.js
@@ -1,13 +1,13 @@
 `use strict`;
 
-const targetId = document.getElementById("targetId");
-const summationId = document.getElementById("summationId");
-const productId = document.getElementById("productId");
-const averageId = document.getElementById("averageId");
+const targetId = document.getElementById('targetId');
+const summationId = document.getElementById('summationId');
+const productId = document.getElementById('productId');
+const averageId = document.getElementById('averageId');
 
 const num1 = parseInt(prompt(`Type a first number:`));
-const num2 = parseInt(prompt(`Type a second number:`))
-const num3 = parseInt(prompt(`Type a third number:`))
+const num2 = parseInt(prompt(`Type a second number:`));
+const num3 = parseInt(prompt(`Type a third number:`));
 
 function summation(num1, num2, num3) {
   return num1 + num2 + num3;
@@ -25,7 +25,8 @@ const summationResult = summation(num1, num2, num3);
 const productResult = product(num1, num2, num3);
 const averageResult = average(num1, num2, num3);
 
-targetId.innerHTML = `The inserted numbers were ${num1}, ${num2} and ${num3}.`
-summationId.innerHTML = `The sum of the numbers is ${summationResult}.`
-productId.innerHTML = `The product of the numbers is ${productResult}.`
-averageId.innerHTML = `The average of the numbers is ${averageResult.toFixed(2)}.`
+targetId.innerHTML = `The inserted numbers were ${num1}, ${num2} and ${num3}.`;
+summationId.innerHTML = `The sum of the numbers is ${summationResult}.`;
+productId.innerHTML = `The product of the numbers is ${productResult}.`;
+averageId.innerHTML = `The average of the numbers is ${averageResult.toFixed(
+    2)}.`;
