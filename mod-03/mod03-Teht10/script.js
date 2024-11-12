4c32c5611b0bdc8ab89cd7475d5b18270e568796
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 23:16:43 +0200

    Mod03 Teht10 Initial commit

--- mod-03/mod03-Teht10/script.js
+++ mod-03/mod03-Teht10/script.js
@@ -1,2 +1,18 @@
 `use strict`;
 
+const form = document.querySelector('#source');
+const firstName = document.querySelector('input[name=firstname]');
+const lastName = document.querySelector('input[name=lastname]');
+const target = document.getElementById('target');
+
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  
+  if (firstName.value === '' || lastName.value === '') {
+    target.innerText = `Please enter both first name and last name`;
+  }
+  else {
+    target.innerText = `Your name is ${firstName.value} ${lastName.value}`;
+  }
+});
+
