893daddf5a5e137b63a0c7ac631a63b02c61838b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 10:25:14 +0200

    Mod03 Teht3 Initial commit

--- mod-03/mod03-Teht3/script.js
+++ mod-03/mod03-Teht3/script.js
@@ -1,2 +1,14 @@
 `use strict`;
 
+const names = ['John', 'Paul', 'Jones'];
+
+function printNames() {
+  const target = document.getElementById('target');
+  for (let i = 0; i < names.length; i++) {
+    const li = document.createElement('li');
+    li.innerHTML = names[i];
+    target.appendChild(li);
+  }
+}
+
+printNames();
