e2c0643ce4e00599e58a28b0c6c78cb986b47844
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 22:15:26 +0200

    Mod03 Teht9 Initial commit

--- mod-03/mod03-Teht9/script.js
+++ mod-03/mod03-Teht9/script.js
@@ -1,2 +1,26 @@
 `use strict`;
 
+function calculate() {
+  const calculation = document.getElementById(`calculation`);
+  
+  const insertedCalculation = calculation.value.trim(); // Remove extra white space
+  let result;
+  
+  if (insertedCalculation.includes(`+`)) {
+    const numbers = insertedCalculation.split(`+`);
+    result = Number(numbers[0]) + Number(numbers[1]);
+  } else if (insertedCalculation.includes(`-`)) {
+    const numbers = insertedCalculation.split(`-`);
+    result = Number(numbers[0]) - Number(numbers[1]);
+  } else if (insertedCalculation.includes(`*`)) {
+    const numbers = insertedCalculation.split(`*`);
+    result = Number(numbers[0]) * Number(numbers[1]);
+  } else if (insertedCalculation.includes(`/`)) {
+    const numbers = insertedCalculation.split(`/`);
+    result = Number(numbers[0]) / Number(numbers[1]);
+  }
+  
+  document.getElementById(`result`).textContent = result;
+}
+
+document.getElementById(`start`).addEventListener(`click`, calculate);
