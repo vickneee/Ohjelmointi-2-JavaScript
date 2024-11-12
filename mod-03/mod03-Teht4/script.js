8c07ac73acc44cc4f4f14e4f1936c7990bdf935d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 16:11:10 +0200

    Mod03 Teht4 Initial commit

--- mod-03/mod03-Teht4/script.js
+++ mod-03/mod03-Teht4/script.js
@@ -1,2 +1,29 @@
 `use strict`;
 
+function studentOption() {
+  const students = [
+    {
+      name: 'John',
+      id: '2345768',
+    },
+    {
+      name: 'Paul',
+      id: '2134657',
+    },
+    {
+      name: 'Jones',
+      id: '5423679',
+    },
+  ];
+  
+  const target = document.getElementById('target');
+  
+  for (let student of students) {
+    const studentNumOption = document.createElement('option');
+    studentNumOption.value = student.id;
+    studentNumOption.textContent = student.name;
+    target.appendChild(studentNumOption);
+  }
+}
+
+studentOption();
