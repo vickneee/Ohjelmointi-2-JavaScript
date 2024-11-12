34f478bb6267881fcedf0271fa8c3d6213d8acef
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-12 18:44:34 +0200

    Mod03 Teht7 Initial commit

--- mod-03/mod03-Teht7/script.js
+++ mod-03/mod03-Teht7/script.js
@@ -1,2 +1,14 @@
 `use strict`;
 
+const trigger = document.getElementById('trigger');
+const originalImage = 'img/picA.jpg';
+const newImage = 'img/picB.jpg';
+const target = document.getElementById('target');
+
+trigger.addEventListener('mouseenter', function() {
+  target.src = newImage;
+});
+
+trigger.addEventListener('mouseleave', function() {
+  target.src = originalImage;
+});
