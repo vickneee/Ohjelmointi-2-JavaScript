e3b41bc2a6da46460b0d19cc0f9b86d3b8d0ebca
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-14 15:01:40 +0200

    Mod04 Teht5 Initial commit

--- mod-04/mod04-Teht5/script.js
+++ mod-04/mod04-Teht5/script.js
@@ -1,2 +1,14 @@
 `use strict`;
 
+async function fetchJoke() {
+  const url = `https://api.chucknorris.io/jokes/random`;
+  try {
+    const response = await fetch(url);
+    const joke = await response.json();
+    console.log(joke.value);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+fetchJoke().catch(error => console.log(error.message));
