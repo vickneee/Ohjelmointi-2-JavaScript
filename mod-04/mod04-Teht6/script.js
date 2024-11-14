cba61d67c054dddfd719e9d2d2c5924cb4373253
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-14 16:06:05 +0200

    Mod04 Teht6 Initial commit

--- mod-04/mod04-Teht6/script.js
+++ mod-04/mod04-Teht6/script.js
@@ -1,2 +1,36 @@
 `use strict`;
 
+const form = document.querySelector(`#search-form`);
+
+form.addEventListener(`submit`, async function(evt) {
+  evt.preventDefault();
+  
+  const query = document.querySelector(`#query`);
+  const results = document.querySelector(`#results`);
+  const url = `https://api.chucknorris.io/jokes/search?query=${query.value}`;
+  
+  // Clear previous results
+  results.innerHTML = ``;
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    let article = document.createElement(`article`);
+    results.appendChild(article);
+    
+    if (data.result.length === 0) {
+      let p = document.createElement(`p`);
+      p.textContent = `No results found.`;
+      article.appendChild(p);
+    }
+    else {
+      data.result.forEach(joke => {
+        let p = document.createElement(`p`);
+        p.textContent = joke.value 
+        article.appendChild(p);
+      });
+    }
+  } catch (error) {
+    console.error(`Error: `, error);
+  }
+});
