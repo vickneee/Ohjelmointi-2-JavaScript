532eb2837126d116dd60d6134f47298ee7c4306a
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-22 09:57:08 +0200

    Mod04 Code corrected

--- mod-04/mod04-Teht2/script.js
+++ mod-04/mod04-Teht2/script.js
@@ -1,18 +1,18 @@
 `use strict`;
 
-// Prevent form from submitting normally and handle with JavaScript
-document.querySelector('form').addEventListener('submit', async function(evt) {
-  evt.preventDefault();
-  
-  // const query = document.getElementById('query').value;
-  const query = document.querySelector("input[name=q]").value;
-  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
-  
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data); // Display results in console
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+// // Prevent form from submitting normally and handle with JavaScript
+// document.querySelector('form').addEventListener('submit', async function(evt) {
+//   evt.preventDefault();
+//
+//   // const query = document.getElementById('query').value;
+//   const query = document.querySelector("input[name=q]").value;
+//   const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+//
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data); // Display results in console
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
